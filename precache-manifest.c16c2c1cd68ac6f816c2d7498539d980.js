51d660c5fb10597dc9d137ecdb21039efea2881b
ravi3177 <2000030682@kluniversity.in>
2023-03-15 10:40:23 +0530

    ok

---
+++ precache-manifest.c16c2c1cd68ac6f816c2d7498539d980.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "617eda9ccb21d16822e6",
+    "url": "/Portfolio/static/css/main.8faac373.chunk.css"
+  },
+  {
+    "revision": "617eda9ccb21d16822e6",
+    "url": "/Portfolio/static/js/main.617eda9c.chunk.js"
+  },
+  {
+    "revision": "9cdbcbf66b620b6edbc1",
+    "url": "/Portfolio/static/css/1.11319134.chunk.css"
+  },
+  {
+    "revision": "9cdbcbf66b620b6edbc1",
+    "url": "/Portfolio/static/js/1.9cdbcbf6.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "18d565f0d00ef63ecf9f0b93601d5c43",
+    "url": "/Portfolio/static/media/Sunil_Resume.18d565f0.pdf"
+  },
+  {
+    "revision": "786c3d70b1366c232b99a5c3a3dec25b",
+    "url": "/Portfolio/static/media/logo.e0975bd0.png"
+  },
+  {
+    "revision": "f1d13f2bda4eb9cac68443854cf4e81d",
+    "url": "/Portfolio/static/media/kindpng_2158189.a3fb932b.png"
+  },
+  {
+    "revision": "fab0d35938e167e1fe57e5e08544b45d",
+    "url": "/Portfolio/static/media/universityLogo.a30d62ed.png"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/Portfolio/static/media/project-cover10.aee0f771.png"
+  },
+  {
+    "revision": "786c3d70b1366c232b99a5c3a3dec25b",
+    "url": "/Portfolio/static/media/personalPhoto.e0975bd0.jpg"
+  },
+  {
+    "revision": "31eb62a553d66f58cacbb195a98ca20c",
+    "url": "/Portfolio/index.html"
+  }
+];
